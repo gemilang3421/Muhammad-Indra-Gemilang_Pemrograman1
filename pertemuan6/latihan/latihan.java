8d354a3b47671d8b711ac9de9a1670728ae451f0
gemilang3421 <anetasimon24@gmail.com>
2026-05-04 11:28:17 +0700

    Update latihan.java

--- pertemuan6/latihan/latihan.java
+++ pertemuan6/latihan/latihan.java
@@ -1,1 +1,25 @@
+package latihan;
 
+class Kendaraan {
+    String merk;
+    String jenis;
+    int tahun;
+
+    void tampilkanInfo() {
+        System.out.println("Merk  : " + merk);
+        System.out.println("Jenis : " + jenis);
+        System.out.println("Tahun : " + tahun);
+    }
+}
+
+public class DataKendaraan {
+    public static void main(String[] args) {
+        Kendaraan kendaraan = new Kendaraan();
+        kendaraan.merk = "Honda";
+        kendaraan.jenis = "Motor";
+        kendaraan.tahun = 2022;
+
+        System.out.println("=== Data Kendaraan ===");
+        kendaraan.tampilkanInfo();
+    }
+}
