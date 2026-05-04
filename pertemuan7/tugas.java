850e26e2167d0076c59e8b1408f47fa45f2188a2
gemilang3421 <anetasimon24@gmail.com>
2026-05-04 11:34:26 +0700

    Update tugas.java

--- pertemuan7/tugas.java
+++ pertemuan7/tugas.java
@@ -1,1 +1,30 @@
+package tugas;
 
+import java.util.Scanner;
+
+public class PembelianTiketBioskopImproved {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Kasir Tiket Bioskop ===");
+        System.out.print("Nama film       : ");
+        String film = input.nextLine();
+
+        System.out.print("Jumlah tiket    : ");
+        int jumlahTiket = input.nextInt();
+
+        System.out.print("Harga per tiket : ");
+        double hargaTiket = input.nextDouble();
+
+        double subtotal = jumlahTiket * hargaTiket;
+        double diskon = jumlahTiket >= 5 ? subtotal * 0.10 : 0;
+        double totalBayar = subtotal - diskon;
+
+        System.out.println("\n=== Struk Pembelian ===");
+        System.out.println("Film       : " + film);
+        System.out.println("Subtotal   : Rp " + String.format("%,.0f", subtotal));
+        System.out.println("Diskon     : Rp " + String.format("%,.0f", diskon));
+        System.out.println("Total Bayar: Rp " + String.format("%,.0f", totalBayar));
+
+        input.close();
+    }
