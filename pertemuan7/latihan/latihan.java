86af4384b41cf88ce646e06950f56d0cbc7b4e61
gemilang3421 <anetasimon24@gmail.com>
2026-05-04 11:31:21 +0700

    Update latihan.java

--- pertemuan7/latihan/latihan.java
+++ pertemuan7/latihan/latihan.java
@@ -1,1 +1,16 @@
+package latihan;
 
+public class PembelianTiketBioskop {
+    public static void main(String[] args) {
+        String film = "Langit Senja";
+        int jumlahTiket = 3;
+        double hargaTiket = 45000;
+        double totalBayar = jumlahTiket * hargaTiket;
+
+        System.out.println("=== Pembelian Tiket Bioskop ===");
+        System.out.println("Film         : " + film);
+        System.out.println("Jumlah Tiket : " + jumlahTiket);
+        System.out.println("Harga Tiket  : Rp " + String.format("%,.0f", hargaTiket));
+        System.out.println("Total Bayar  : Rp " + String.format("%,.0f", totalBayar));
+    }
+}
