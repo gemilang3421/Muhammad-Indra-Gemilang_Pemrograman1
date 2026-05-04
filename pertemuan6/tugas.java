54650aff5acb1c139e9bc0aa9103483ff2ccc0d6
gemilang3421 <anetasimon24@gmail.com>
2026-05-04 11:30:10 +0700

    Update tugas.java

--- pertemuan6/tugas.java
+++ pertemuan6/tugas.java
@@ -1,1 +1,36 @@
+package tugas;
 
+class KendaraanDetail {
+    String merk;
+    String jenis;
+    int tahun;
+    double harga;
+
+    KendaraanDetail(String merk, String jenis, int tahun, double harga) {
+        this.merk = merk;
+        this.jenis = jenis;
+        this.tahun = tahun;
+        this.harga = harga;
+    }
+
+    int hitungUsia(int tahunSekarang) {
+        return tahunSekarang - tahun;
+    }
+
+    void tampilkanInfo() {
+        System.out.println("Merk  : " + merk);
+        System.out.println("Jenis : " + jenis);
+        System.out.println("Tahun : " + tahun);
+        System.out.println("Harga : Rp " + String.format("%,.0f", harga));
+    }
+}
+
+public class DataKendaraanImproved {
+    public static void main(String[] args) {
+        KendaraanDetail kendaraan = new KendaraanDetail("Toyota", "Mobil", 2020, 235000000);
+
+        System.out.println("=== Detail Kendaraan ===");
+        kendaraan.tampilkanInfo();
+        System.out.println("Usia  : " + kendaraan.hitungUsia(2026) + " tahun");
+    }
+}
